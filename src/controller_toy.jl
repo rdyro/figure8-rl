6dabbf4dde04bcd02cfba8deda065032599a2cda
Javier Yu <javiercyu@gmail.com>
2018-11-17 14:01:59 -0800

    created toy controller file

---
+++ src/controller_toy.jl
@@ -0,0 +1,15 @@
+using LinearAlgebra
+
+function controller_toy!(u::AbstractArray{Float64},
+                             x::AbstractArray{Float64},
+                             dx::AbstractArray{Float64},
+			     agent_world::Pair{Agent, World}, t::Float64)
+  
+
+
+
+
+
+
+end
+			 
