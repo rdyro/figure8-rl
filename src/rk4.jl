7cd830822c5319323796d1bd9033ee503c1371ea
robertdyro <robert.dyro@gmail.com>
2018-11-16 22:15:35 -0800

    refactored the simulation code

--- src/rk4.jl
+++ src/rk4.jl
@@ -1,4 +1,4 @@
-function rk4!(f!::Function, x::Array, p, t::Number, h::Number)::Nothing
+function rk4!(f!::Function, x::Array, p, t::Number, tn::Number, h::Number)
   n = length(x)
 
   mem = fill(zero(eltype(x)), n * 5)
@@ -7,29 +7,36 @@ function rk4!(f!::Function, x::Array, p, t::Number, h::Number)::Nothing
   k3 = view(mem, (2 * n + 1):(3 * n))
   k4 = view(mem, (3 * n + 1):(4 * n))
   work = view(mem, (4 * n + 1):(5 * n))
+  while t < tn
+    for i = 1:n
+      work[i] = x[i]
+    end
+    f!(k1, work, p, t)
 
-  for i = 1:n
-    work[i] = x[i]
-  end
-  f!(k1, work, p, t)
+    for i = 1:n
+      work[i] = x[i] + 0.5 * h * k1[i]
+    end
+    f!(k2, work, p, t + 0.5 * h)
 
-  for i = 1:n
-    work[i] = x[i] + 0.5 * h * k1[i]
-  end
-  f!(k2, work, p, t + 0.5 * h)
+    for i = 1:n
+      work[i] = x[i] + 0.5 * h * k2[i]
+    end
+    f!(k3, work, p, t + 0.5 * h)
 
-  for i = 1:n
-    work[i] = x[i] + 0.5 * h * k2[i]
-  end
-  f!(k3, work, p, t + 0.5 * h)
+    for i = 1:n
+      work[i] = x[i] + h * k3[i]
+    end
+    f!(k4, work, p, t + h)
 
-  for i = 1:n
-    work[i] = x[i] + h * k3[i]
-  end
-  f!(k4, work, p, t + h)
+    for i = 1:n
+      x[i] += h / 6.0 * (k1[i] + 2.0 * k2[i] + 2.0 * k3[i] + k4[i])
+    end
 
-  for i = 1:n
-    x[i] += h / 6.0 * (k1[i] + 2.0 * k2[i] + 2.0 * k3[i] + k4[i])
+    if tn - t > h
+      t += h
+    else
+      t = tn
+    end
   end
 
   return
--- src/sim.jl
+++ src/sim.jl
@@ -3,26 +3,7 @@ using LinearAlgebra
 include("vis.jl")
 include("rk4.jl")
 
-function binary_search(sorted_x::Array{T}, el::T) where T
-  a = 1
-  b = length(sorted_x)
-  if el >= sorted_x[b]
-    return b
-  end
-
-  m = div(a + b, 2)
-  while b - a > 1
-    if sorted_x[m] > el
-      b = m
-    else
-      a = m
-    end
-    m = div(a + b, 2)
-  end
-
-  return m
-end
-
+# Type Definitions ############################################################
 struct Path
   X::Array{Float64}
   Y::Array{Float64}
@@ -38,60 +19,97 @@ struct Path
   S::Array{Float64}
 end
 
-function main()
-  context = Vis.setup()
+struct Road
+  path::Path
+  width::Float64
+  road::Union{Vis.RenderObject, Nothing}
+end
+Road(path, width) = Road(path, width, nothing)
 
-  path = make_figure8()
+mutable struct Agent
+  id::Int
+  x::Array{Float64, 1}
+  dynamics!::Function
+  controller!::Function
+  car::Union{Vis.RenderObject, Nothing}
+end
+Agent(id, x) = Agent(id, x, default_dynamics!, default_controller!, nothing)
+Agent(id, x, car::Vis.RenderObject) = Agent(id, x, default_dynamics!, 
+                                            default_controller!, car)
 
-  road = Vis.make_road(context, path.X, path.Y, 0.1)
+struct World
+  road::Road
+  agents::Array{Agent, 1}
+end
+World(road::Road) = World(road, Agent[])
 
-
-  u = [0.0; 1.0; 0.0]
-  car = Vis.make_car(context)
-  Vis.car_lights!(car, false)
-
-  u2 = [0.0; 1.0; 0.05]
-  car2 = Vis.make_car(context, [1.0, 1.0, 0.0])
-  Vis.car_lights!(car2, false)
-
-  u3 = [0.0; 1.0; -0.05]
-  car3 = Vis.make_car(context, [1.0, 0.0, 1.0])
-  Vis.car_lights!(car3, false)
-
-  window = true
-  frame = 0
-  while window
-    rk4!(car_move!, u, path, 0, 0.01)
-    rk4!(car_move!, u2, path, 0, 0.01)
-    rk4!(car_move!, u3, path, 0, 0.01)
-
-    (x, y) = sp2xy(u[1], u[3], path)
-    (dx, dy) = sp2dxdy(u[1], u[3], path)
-    th = atan(dy, dx) - pi / 2
-
-    car.T = (Vis.translate_mat(x, y) * Vis.rotate_mat(th) * 
-             Vis.scale_mat(0.3, 0.3))
-
-    (x, y) = sp2xy(u2[1], u2[3], path)
-    (dx, dy) = sp2dxdy(u2[1], u2[3], path)
-    th = atan(dy, dx) - pi / 2
-
-    car2.T = (Vis.translate_mat(x, y) * Vis.rotate_mat(th) * 
-             Vis.scale_mat(0.3, 0.3))
-
-    (x, y) = sp2xy(u3[1], u3[3], path)
-    (dx, dy) = sp2dxdy(u3[1], u3[3], path)
-    th = atan(dy, dx) - pi / 2
-
-    car3.T = (Vis.translate_mat(x, y) * Vis.rotate_mat(th) * 
-             Vis.scale_mat(0.3, 0.3))
-
-    window = Vis.visualize(context, [road, car, car2, car3])
-
-    frame += 1
+# Custom Rendering ############################################################
+function update_renderer(agent::Agent, road::Road)
+  if agent.car == nothing
+    return
   end
+
+  (x, y) = sp2xy(agent.x[1], agent.x[3], road.path)
+  (sx, sy) = sp2sxsy(agent.x[1], agent.x[3], road.path)
+  th = atan(sy, sx) - pi / 2
+
+  agent.car.T = (Vis.translate_mat(x, y) * Vis.rotate_mat(th) * 
+                 Vis.scale_mat(0.3, 0.3))
+  return
 end
 
+# Dynamics and Control ########################################################
+function default_controller!(u::AbstractArray{Float64}, 
+                             x::AbstractArray{Float64}, 
+                             dx::AbstractArray{Float64}, 
+                             agent_world::Pair{Agent, World}, t::Float64)
+  u[1] = 0 # no braking nor accelerating
+  u[2] = -dx[3] # counteract lateral movement
+
+  return
+end
+
+function default_dynamics!(dx, x, agent_world, t)
+  agent = agent_world.first
+  world = agent_world.second
+
+  # extract states
+  s = x[1]
+  ds = x[2]
+  p = x[3]
+
+  # get local interpolated path curvature
+  path = world.road.path
+
+  s = rem(s, path.S[end])
+  idx = binary_search(path.S, s)
+  R = path.R[idx]
+  R1 = path.R[idx + 1]
+  S = path.S[idx]
+  S1 = path.S[idx + 1]
+  r = (R1 - R) / (S1 - S) * (s - S) + R
+
+  # compute rates
+  ds *= isnan(r / (r - p)) ? 1 : r / (r - p)
+  dds = 0.0
+  dp = isnan(ds^2 / r) ? 0.0 : 1e-2 * -ds^2 / r
+
+  dx[1] = ds
+  dx[2] = dds
+  dx[3] = dp
+
+  # controller acts
+  u = fill(0.0, 2)
+  agent.controller!(u, x, dx, agent_world, t)
+
+  dx[1] += u[1]
+  dx[3] += u[2]
+
+  # done
+  return
+end
+
+# Utility Functions ###########################################################
 function sp2xy(s::Float64, p::Float64, path)
   s = rem(s, path.S[end])
   idx = binary_search(path.S, s)
@@ -120,7 +138,7 @@ function sp2xy(s::Float64, p::Float64, path)
   return (x, y)
 end
 
-function sp2dxdy(s::Float64, p::Float64, path)
+function sp2sxsy(s::Float64, p::Float64, path)
   s = rem(s, path.S[end])
   idx = binary_search(path.S, s)
 
@@ -133,37 +151,33 @@ function sp2dxdy(s::Float64, p::Float64, path)
   S = path.S[idx]
   S1 = path.S[idx + 1]
 
-  dx = (dX1 - dX) / (S1 - S) * (s - S) + dX
-  dy = (dY1 - dY) / (S1 - S) * (s - S) + dY
+  sx = (dX1 - dX) / (S1 - S) * (s - S) + dX
+  sy = (dY1 - dY) / (S1 - S) * (s - S) + dY
 
-  return (dx, dy)
+  return (sx, sy)
 end
 
-function car_move!(dx, x, path, t)
-  s = x[1]
-  ds = x[2]
-  p = x[3]
+function binary_search(sorted_x::Array{T}, el::T) where T
+  a = 1
+  b = length(sorted_x)
+  if el >= sorted_x[b]
+    return b
+  end
 
-  s = rem(s, path.S[end])
-  idx = binary_search(path.S, s)
-  R = path.R[idx]
-  R1 = path.R[idx + 1]
-  S = path.S[idx]
-  S1 = path.S[idx + 1]
-  r = (R1 - R) / (S1 - S) * (s - S) + R
+  m = div(a + b, 2)
+  while b - a > 1
+    if sorted_x[m] > el
+      b = m
+    else
+      a = m
+    end
+    m = div(a + b, 2)
+  end
 
-  ds *= isnan(r / (r - p)) ? 1 : r / (r - p)
-  dds = 0
-  dp = 0
-
-  dx[1] = ds
-  dx[2] = dds
-  dx[3] = dp
-
-  return
+  return m
 end
 
-function make_figure8()
+function make_figure8_path()
   X = Float64[]
   Y = Float64[]
   N = 100
@@ -215,7 +229,6 @@ function make_figure8()
   end
   dX[end] = X[end] - X[end-1]
   dY[end] = Y[end] - Y[end-1]
-
 
   # second derivative
   ddX = similar(dX)
@@ -269,6 +282,56 @@ function remove_duplicates(x::Array{Float64}, y::Array{Float64})
       len = length(x)
     end
     i += 1
+  end
+end
+
+# Main Routine ################################################################
+function main()
+  # load the graphical context (OpenGL handle, the graphical window, etc.)
+  context = Vis.setup()
+
+  # make the road
+  path = make_figure8_path()
+  road_width = 0.1
+  road = Road(path, road_width, 
+              Vis.make_road(context, path.X, path.Y, road_width))
+
+  # make the agents
+  agent1 = Agent(1, [0.0; 1; 0], Vis.make_car(context))
+  Vis.car_lights!(agent1.car, false)
+
+  agent2 = Agent(1, [0.0; 1; 0.05], Vis.make_car(context, [1.0, 1.0, 0.0]))
+  Vis.car_lights!(agent2.car, false)
+
+  agent3 = Agent(1, [0.0; 1; -0.05], Vis.make_car(context, [1.0, 0.0, 1.0]))
+  Vis.car_lights!(agent3.car, false)
+
+  # make the world
+  world = World(road, [agent1, agent2, agent3])
+
+  window = true
+  h = 1e-2
+  t0 = time_ns()
+  oldt = (time_ns() - t0) / 1e9
+  while window
+    t = (time_ns() - t0) / 1e9
+
+    to_visualize = Vis.RenderObject[]
+    if world.road.road != nothing
+      push!(to_visualize, world.road.road)
+    end
+
+    for agent in world.agents
+      rk4!(agent.dynamics!, agent.x, Pair(agent, world), oldt, t, h)
+      update_renderer(agent, world.road)
+      if agent.car != nothing
+        push!(to_visualize, agent.car)
+      end
+    end
+
+    window = Vis.visualize(context, to_visualize)
+
+    oldt = t
   end
 end
 
