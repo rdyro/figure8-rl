2fccc5e4d9c0f519fc432aa6f30e23e382588dfc
Javier Yu <javiercyu@gmail.com>
2018-12-05 18:56:49 -0800

    removing dx from parametrize

--- src/adv/adv.jl
+++ src/adv/adv.jl
@@ -19,9 +19,7 @@ using LinearAlgebra
 
 export predict_collision
 
-function replan_adv(agent_world::Pair{Agent, World}, t::Float64)
-  world = agent_world.second
-  agent_self = agent_world.first
+function replan_adv(agent_self::Agent, world::World, t::Float64)
   target_v = world.road.path.S[end] / 15 # target velocity is around the track in 15 seconds
 
   d_min = Inf
@@ -33,7 +31,7 @@ function replan_adv(agent_world::Pair{Agent, World}, t::Float64)
       continue
     end
 
-    (d, t_c, c_type) = predict_collision(agent_self.x, agent_self, agent.x, agent, world, t)
+    (d, t_c, c_type) = predict_collision(agent_self.x, agent.x, world, t)
     
     if c_type != 0 && d < d_min
       d_min = d
@@ -41,7 +39,7 @@ function replan_adv(agent_world::Pair{Agent, World}, t::Float64)
     end
   end
 
-	return pomdp.sample_adv_a(agent.custom[5], c_type_closest)
+	return pomdp.sample_adv_a(agent_self.custom[5], c_type_closest)
 
 end
 
@@ -55,8 +53,7 @@ function empty_controller!(x::AbstractArray{Float64},
 end
 
 function parametrize(x::AbstractArray{Float64},
-                        dx::AbstractArray{Float64},
-                        world::World)
+                     world::World)
 
   (sx, sy) = sim.sp2sxsy(x[1], x[3], world.road.path)
 	th = atan(sy, sx)
@@ -66,10 +63,9 @@ function parametrize(x::AbstractArray{Float64},
   dds = dx[2]
 
   p = x[3]
-  dp = dx[3]
 
-  v_x = ds*cos(th) + dp*sin(th)
-  v_y = ds*sin(th) + dp*cos(th)
+  v_x = ds*cos(th)
+	v_y = ds*sin(th)
 
   a_x = dds*cos(th)
   a_y = dds*sin(th)
@@ -102,8 +98,8 @@ function closest_approach(p1::AbstractArray{Float64}, p2::AbstractArray{Float64}
   return (v1, v2, t)
 end
 
-function predict_collision(x_self::Array{Float64}, agent_self::Agent,
-													 x_opp::Array{Float64}, agent_opp::Agent,
+function predict_collision(x_self::Array{Float64},
+													 x_opp::Array{Float64},
 													world::World,
 													time::Float64=0.0)
 	# Parametrize both agents
@@ -115,22 +111,9 @@ function predict_collision(x_self::Array{Float64}, agent_self::Agent,
 	# 1 - agent_self crashes into agent_opp
 	# 2 - agent_self is crashed into by agent opp
 	
-	# Compute dx for each agent
-	dx_self = fill(0.0, 3)
-	f_self = agent_self.controller!
-	agent_self.controller! = empty_controller!
-	agent_self.dynamics!(dx_self, x_self, Pair(agent_self, world), time)
-	agent_self.controller! = f_self
-	
-	dx_opp = fill(0.0, 3)
-	f_opp = agent_opp.controller!
-	agent_opp.controller! = empty_controller!
-	agent_opp.dynamics!(dx_opp, x_opp, Pair(agent_opp, world), time)
-	agent_opp.controller! = f_opp
-
 	# Parametrize both agents
-	(P_self, th_self) = parametrize(dx_self, x_self, world)
-	(P_opp, th_opp) = parametrize(dx_opp, x_opp, world)
+	(P_self, th_self) = parametrize(x_self, world)
+	(P_opp, th_opp) = parametrize(x_opp, world)
 
 	# Compute Closest approach collision
 	(vc_self, vc_opp, t_c) = closest_approach(P_self, P_opp)
